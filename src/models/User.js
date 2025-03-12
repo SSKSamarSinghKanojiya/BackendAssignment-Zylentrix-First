4139286aa1e7c75479de6aca0b1ed08219585da0
Samar Singh Kanojiya <ssksamarsinghkanojiya.com@gmail.com>
2025-03-12 19:10:30 +0530

    model file

--- src/models/User.js
+++ src/models/User.js
@@ -5,9 +5,6 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     age: { type: Number, required: true },
-  },
-  {
-    timestamps: true,
   }
 );
 
